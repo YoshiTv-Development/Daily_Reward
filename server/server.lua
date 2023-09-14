548baf1d5624fe4610599571beed6be3468e68b2
YoshiTv-Development <142677543+YoshiTv-Development@users.noreply.github.com>
2023-09-14 20:53:53 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,4 +1,9 @@
 local random = Config.Riconpensa.Amount
+local randomitem = Config.Riconpensa.Oggetti
 RegisterServerEvent("reward", function(item)
+    for k, v in pairs(ItemRicevere) do
+        exports.ox_inventory:AddItem(source, v, randomitem, false, false)
+    end
     exports.ox_inventory:AddItem(source, Config.Riconpensa.Item, random, false, false)
 end)
+
